447094cde732254114a6f33e3db62b847c13749a
Aria Nikzad <a.c.nikzad@gmail.com>
2022-05-03 17:43:19 -0700

    completed project 2

--- 2_project.rb
+++ 2_project.rb
@@ -1,4 +1,4 @@
-dial_book={
+dial_book = {
   "newyork" => "212",
   "newbrunswick" => "732",
   "edison" => "908",
@@ -13,15 +13,26 @@ dial_book={
 
 # Get city names from the hash
 def get_city_names (somehash)
-# Write code here
+  somehash.keys
 end
 
 # Get area code based on given hash and key
 def get_area_code(somehash, key)
-# Write code here
+    somehash[key]
 end
 
 # Execution flow
 loop do
-# Write code here
+  puts "Do you want to lookup an area code based on a city name? (Y/N)"
+  answer = gets.chomp.downcase
+  break if answer != "y"
+  puts "Which city do you want to lookup areacode for?"
+  puts get_city_names(dial_book)
+  puts "Enter your selection"
+  prompt = gets.chomp
+  if dial_book.include?(prompt)
+    puts "The area code for #{prompt} is #{get_area_code(dial_book, prompt)}"
+  else
+  puts "You entered an invalid city name"
+  end
 end
